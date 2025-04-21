8637dbd404d984faa2baaee084ce324ecbf6bce9
Ahmad Wali <ahmadwali2505@gmail.com>
2025-04-21 13:08:28 -0400

    Add files via upload

---
+++ CP164/wali6947_a06/t02.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Ahmad Wali
+ID:      169036947
+Email:   wali6947l@mylaurier.ca
+__updated__ = "2024-02-17"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+from Priority_Queue_linked import Priority_Queue
+# Initialize
+q1 = Priority_Queue()
+q2 = Priority_Queue()
+
+empty_state1 = q1.is_empty()
+empty_state2 = q2.is_empty()
---
+++ CP164/wali6947_a06/t03.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Ahmad Wali
+ID:      169036947
+Email:   wali6947l@mylaurier.ca
+__updated__ = "2024-02-17"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+from Deque_linked import Deque
+# Initialize
+q1 = Deque()
+q2 = Deque()
+
+empty_state1 = q1.is_empty()
+empty_state2 = q2.is_empty()
